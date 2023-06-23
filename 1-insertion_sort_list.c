31094f05bd7d4eb5d193c87f9c6c1bd427408fcf
Katekani Madingana <madinganakn@gmail.com>
2023-06-23 07:45:16 -0700

    all the c files and header file

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "sort.h"
+#include <stdlib.h>
+
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *node1;
+	listint_t *node2;
+	listint_t *hold;
+	listint_t *hold1
+
+	if (!list or !*list)
+		return;
+
+	node1 = *list;
+	node2 = current->next;
+	
+
+	
+}
