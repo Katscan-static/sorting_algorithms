702be2f5a975ec7abd0e8a87450c72c4c43d4b32
Charlessnow <ekekwecharles50@gmail.com>
2023-06-24 08:12:01 -0700

    updated insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -12,7 +12,7 @@ void insertion_sort_list(listint_t **list)
 	listint_t *current = *list;
 	listint_t *nextNode = NULL;
 
-	if (!(*list) || !(*list)->next)
+	if (!list || !(*list) || !(*list)->next)
 		return;
 
 	while (current)
